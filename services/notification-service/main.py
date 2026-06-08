3047fc8a11b50101e2f1f148d7114a4e250ab492
Lucky Virgiawan Dwi Widodo <luckyvirgiawan24@gmail.com>
2026-06-09 01:58:11 +0700

    fix: resolve prometheus duplicate timeseries and update uvicorn execution method

--- services/notification-service/main.py
+++ services/notification-service/main.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI
-from prometheus_client import make_asgi_app
-import uvicorn
+from prometheus_client import make_asgi_app, Counter
+from config import settings
 
-app = FastAPI(title="MicroMail Notification Service")
+app = FastAPI(title=settings.PROJECT_NAME)
 
-# Menambahkan endpoint metrics untuk Prometheus (Tugas Tracing)
+# --- Observability: Prometheus Metrics ---
+# Hapus "_total" pada nama metrik, Prometheus akan menambahkannya secara otomatis
+REQUEST_COUNT = Counter("api_requests", "Total requests", ["method", "endpoint"])
+
+# Menambahkan endpoint /metrics
 metrics_app = make_asgi_app()
 app.mount("/metrics", metrics_app)
 
 @app.get("/health")
 async def health_check():
+    # Menambah hitungan metrik setiap kali endpoint ini dipanggil
+    REQUEST_COUNT.labels(method="GET", endpoint="/health").inc()
     return {"status": "healthy", "service": "notification-service"}
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8005, reload=True)
